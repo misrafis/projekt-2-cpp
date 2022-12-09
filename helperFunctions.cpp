07cf0499ed8c7525855880d5059947dc7fb437ef
Rafał Patrzałek <rafal.patrzalek02@gmail.com>
2022-12-09 16:02:38 +0100

    Finalna wersja programu

--- helperFunctions.cpp
+++ helperFunctions.cpp
@@ -28,11 +28,32 @@ void getArrayFromFile(int array[], string fileName) {
     if(File.is_open()) {
         File >> arraySize;
         while(counter < arraySize + 1) {
-            cout << counter << ": essa" << endl;
+            cout << counter << endl;
             File >> array[counter - 1];
             counter++;
         }
     }
+    File.close();
+}
+
+void writeArrayToFile(int array[], int arraySize, auto duration, string sortType, string fileName) {
+    if(sortType == "mergeSort") {
+        sortType = "scalanie";
+    }
+    if(sortType == "insertionSort") {
+        sortType = "wstawianie";
+    }
+    ofstream File;
+    File.open(fileName);
+    File.clear();
+    if(File.is_open()) {
+        File << "Sortowanie przez "<< sortType <<" dla tablicy o " << arraySize << " elementow - czas: " << duration << "ms" << endl;
+        File << arraySize << endl;
+        for(int i = 0; i < arraySize; i++) {
+            File << array[i] << " ";
+        }
+    }
+    File.close();
 }
 
 void printVector(const std::vector<string> &vector) {
--- main.cpp
+++ main.cpp
@@ -8,34 +8,42 @@
 using namespace std;
 using namespace chrono;
 
+int *array;
+
 int main() {
-    vector<string> examples = {};
+    // Aby tablica z danego pliku została posortowana należy w wektorze input_names podać nazwy plików.
+    vector<string>inputNames = {};
 
-    for (const auto &item : examples) {
-        int arraySize = getArraySize(item);
-        int array[arraySize];
+    for (const auto &inputName : inputNames) {
+        int i = 0;
+        int arraySize = getArraySize(inputName);
+        auto arraySizeString = std::to_string(arraySize);
+        cout << arraySize << endl;
+        array = new int[arraySize];
 
-        getArrayFromFile(array, item);
+        getArrayFromFile(array, inputName);
 
         auto mergeSortStart = high_resolution_clock::now();
-        mergeSort(array,0, arraySize - 1);
+        mergeSort(array, 0, arraySize - 1);
         auto mergeSortStop = high_resolution_clock::now();
 
-        getArrayFromFile(array, item);
+        getArrayFromFile(array, inputName);
 
         auto insertionSortStart = high_resolution_clock::now();
         insertionSort(array, arraySize);
         auto insertionSortStop = high_resolution_clock::now();
 
-        auto durationMergeSort = duration_cast<milliseconds>(mergeSortStop - mergeSortStart);
-        auto durationInsertionSort = duration_cast<milliseconds>(insertionSortStop - insertionSortStart);
+        auto durationMergeSort = duration_cast<milliseconds>(mergeSortStop - mergeSortStart).count();
+        auto durationInsertionSort = duration_cast<milliseconds>(insertionSortStop - insertionSortStart).count();
 
-        cout << "Sortowanie przez scalanie dla tablicy o "<< arraySize <<" elementow - czas: " << durationMergeSort.count() << endl;
-//        printArray(arrayForMergeSort, arraySize);
+        writeArrayToFile(array, arraySize, durationMergeSort, "mergeSort", "output" + arraySizeString + "mergeSort.txt");
+        writeArrayToFile(array, arraySize, durationInsertionSort, "insertionSort", "output" + arraySizeString + "insertionSort.txt");
+
+        cout << "Sortowanie przez scalanie dla tablicy o " << arraySize << " elementow - czas: " << durationMergeSort << endl;
         cout << endl;
-        cout << "Sortowanie przez wstawianiedla tablicy o "<< arraySize <<" elementow - czas: " << durationInsertionSort.count() << endl;
-//        printArray(arrayForInsertionSort, arraySize);
+        cout << "Sortowanie przez wstawianiedla tablicy o " << arraySize << " elementow - czas: " << durationInsertionSort << endl;
         cout << endl;
+        delete[] array;
     }
     return 0;
 }
