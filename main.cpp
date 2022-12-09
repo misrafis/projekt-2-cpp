5acc5aedbf7de6ac58e5f411b6bcf3ecc46be7ba
Rafał Patrzałek <rafal.patrzalek02@gmail.com>
2022-12-09 02:01:21 +0100

    Drobne poprawki

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <vector>
 #include "mergeSort.cpp"
 #include "insertionSort.cpp"
 #include "helperFunctions.cpp"
 using namespace std;
+using namespace chrono;
 
 int main() {
-    vector<string> examples = {"input.txt", "input10.txt", "input1000.txt"};
+    vector<string> examples = {};
 
     for (const auto &item : examples) {
         int arraySize = getArraySize(item);
@@ -16,13 +19,22 @@ int main() {
         getArrayFromFile(arrayForInsertionSort, item);
         getArrayFromFile(arrayForMergeSort, item);
 
+        auto mergeSortStart = high_resolution_clock::now();
         mergeSort(arrayForMergeSort,0, arraySize - 1);
-        insertionSort(arrayForInsertionSort, arraySize);
+        auto mergeSortStop = high_resolution_clock::now();
 
-        cout << "Sortowanie dla " << item << endl;
-        printArray(arrayForMergeSort, arraySize);
+        auto insertionSortStart = high_resolution_clock::now();
+        insertionSort(arrayForInsertionSort, arraySize);
+        auto insertionSortStop = high_resolution_clock::now();
+
+        auto durationMergeSort = duration_cast<milliseconds>(mergeSortStop - mergeSortStart);
+        auto durationInsertionSort = duration_cast<milliseconds>(insertionSortStop - insertionSortStart);
+
+        cout << "Sortowanie przez scalanie dla tablicy o "<< arraySize <<" elementow - czas: " << durationMergeSort.count() << endl;
+//        printArray(arrayForMergeSort, arraySize);
         cout << endl;
-        printArray(arrayForInsertionSort, arraySize);
+        cout << "Sortowanie przez wstawianiedla tablicy o "<< arraySize <<" elementow - czas: " << durationInsertionSort.count() << endl;
+//        printArray(arrayForInsertionSort, arraySize);
         cout << endl;
     }
     return 0;
